6be295ff7890fc9a1e723665b5825acc5df00f04
thirumurugan7 <thiru82003@gmail.com>
2023-04-18 16:07:10 +0530

    l

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
@@ -13,9 +12,6 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      fontFamily: {
-        Inter: ["Inter Sans", ...defaultTheme.fontFamily.sans],
       },
     },
   },
